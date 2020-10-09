a4dce96db064f13f5ab362d5b91d6aa73a5a9c14
ssshoshi <jlschultz88@gmail.com>
2020-10-09 10:23:05 -0400

    update popup, add card shadows

--- dist/js/background.js
+++ dist/js/background.js
@@ -1,18 +1,16 @@
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   var url =
     "https://www.zillow.com/search/GetSearchPageState.htm?searchQueryState=" +
     encodeURIComponent(request.urlParams);
   fetch(url)
-    .then(response => response.json())
-    .then(data => sendResponse(data.searchResults.mapResults))
-    .catch(error => console.log(error));
+    .then((response) => response.json())
+    .then((data) => sendResponse(data.searchResults.mapResults))
+    .catch((error) => console.log(error));
   return true;
 });
 
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
   if (request.msg === "ok") {
-  chrome.tabs.create({ url: "/house-finder.html" });
+    chrome.tabs.create({ url: "/homescanner.html" });
   }
 });
-
-

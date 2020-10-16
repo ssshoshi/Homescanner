45881fa2dbc2d41f3bf9f19e1d1b53832df73e07
ssshoshi <jlschultz88@gmail.com>
2020-10-15 22:49:04 -0400

    validate coordinates, allow popup enter key

--- dist/js/popup.js
+++ dist/js/popup.js
@@ -1,8 +1,34 @@
-document.querySelector(".btn").onclick = function() {
-  var d = document.querySelector("#coords").value;
-  chrome.storage.local.set({ data: d }, function() {
-    if (chrome.runtime.error) {
-      console.log("Runtime error.");
-    }
-  });
+ck_lat = /^(-?[1-8]?\d(?:\.\d{1,18})?|90(?:\.0{1,18})?)$/;
+ck_lon = /^(-?(?:1[0-7]|[1-9])?\d(?:\.\d{1,18})?|180(?:\.0{1,18})?)$/;
+
+const checkCoords = (lat, lon) => {
+  let validLat = ck_lat.test(lat);
+  let validLon = ck_lon.test(lon);
+  if (validLat && validLon) {
+    return true;
+  } else {
+    return false;
+  }
 };
+
+document.querySelector(".btn").onclick = function (e) {
+  var d = document.querySelector("#coords").value;
+  let latLong = d.split(/,\s+/);
+  if (checkCoords(latLong[0], latLong[1])) {
+    chrome.storage.local.set({ data: d }, function () {
+      if (chrome.runtime.error) {
+        console.log("Runtime error.");
+      }
+    });
+  } else {
+    e.preventDefault();
+  }
+};
+
+const enter = document.querySelector("#enter");
+const input = document.querySelector("#coords");
+coords.addEventListener("keypress", (e) => {
+  if (input.length > e.key === "Enter") {
+    enter.click();
+  }
+});
